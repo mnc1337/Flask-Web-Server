0abc5676a5ade9529cb3e12228a268caf83a06e3
mnc1337 <olehworking@gmail.com>
2025-06-11 15:45:53 +0300

    Main file (app.py) changed

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
-from flask import Flask, render_template, request, jsonify, send_from_directory, redirect, url_for
+from flask import Flask, render_template, request, jsonify, send_from_directory, redirect, url_for, Response
 from helpful_files.config import *
 import os
 from send2trash import send2trash
 from flask_limiter import Limiter
 from flask_limiter.util import get_remote_address
+from typing import Tuple
 
 app = Flask(__name__)
 
@@ -12,7 +13,7 @@ app.config.from_mapping(
     MAX_CONTENT_LENGTH = 5000 * 1024 * 1024,
 )
 
-def limit_func():
+def limit_func() -> Response:
     return get_remote_address()
 
 limiter = Limiter(
@@ -29,41 +30,41 @@ def home():
     return redirect(url_for("check_key"))
 
 @app.route("/check_key")
-def check_key():
+def check_key() -> Response:
     return render_template("check_key.html")
 
 @app.route("/upload_redirect_download")
-def upload_redirect_download():
+def upload_redirect_download() -> Response:
     return render_template("upload_redirect_download.html")
 
 @app.route("/upload_redirect_delete")
-def upload_redirect_delete():
+def upload_redirect_delete() -> Response:
     return render_template("upload_redirect_delete.html")
 
 @app.route("/upload_redirect_get_all")
-def upload_redirect_get_all():
+def upload_redirect_get_all() -> Response:
     return render_template("upload_redirect_get_all.html")
 
 @app.route("/upload_redirect_check_shortcuts")
-def upload_redirect_check_shortcuts():
+def upload_redirect_check_shortcuts() -> Response:
     return render_template("upload_redirect_check_shortcuts.html")
 
 @app.route("/<string:enter_key>/get_enter_key")
-def get_enter_key(enter_key):
+def get_enter_key(enter_key) -> Response:
     if enter_key == ENTER_KEY:
         return jsonify(key=ENTER_KEY)
     else:
         return jsonify(error="Enter key is incorrect"), 400
     
 @app.route("/<string:delete_key>/get_delete_key")
-def get_delete_key(delete_key):
+def get_delete_key(delete_key) -> Response:
     if delete_key == DELETE_KEY:
         return jsonify(key=DELETE_KEY)
     else:
         return jsonify(error="Delete key is incorrect"), 400
 
 @app.route("/<string:enter_key>/upload", methods=["GET", "POST"])
-def upload(enter_key):
+def upload(enter_key) -> Response:
     if enter_key != ENTER_KEY:
         return redirect(url_for("home"))
     
@@ -93,14 +94,14 @@ def upload(enter_key):
         return render_template("file_not_found_error.html"), 404
     
 @app.route("/<string:enter_key>/upload_shortcuts")
-def upload_shortcuts(enter_key):
+def upload_shortcuts(enter_key) -> Response:
     if enter_key == ENTER_KEY:
         return render_template("upload_shortcuts.html")
     else:
         return render_template("key_error.html"), 400
 
 @app.route("/<string:enter_key>/download/<string:filename>", methods=["GET", "POST"])
-def download_by_filename(enter_key, filename):
+def download_by_filename(enter_key, filename) -> Response:
     if enter_key == ENTER_KEY:
         full_path = os.path.join(UPLOAD_FOLDER, filename)
         if os.path.exists(full_path):
@@ -117,7 +118,7 @@ def download_by_filename(enter_key, filename):
         return render_template("key_error.html"), 400
     
 @app.route("/<string:delete_key>/delete/<string:filename>", methods=["GET", "DELETE"])
-def delete_by_filename(delete_key, filename):
+def delete_by_filename(delete_key, filename) -> Response | str:
     if delete_key == DELETE_KEY:
         full_path = os.path.join(UPLOAD_FOLDER, filename)
         if os.path.exists(full_path):
@@ -135,7 +136,7 @@ def delete_by_filename(delete_key, filename):
         return render_template("key_error.html"), 400
 
 @app.route("/<string:enter_key>/get_all")
-def get_all(enter_key):
+def get_all(enter_key) -> str:
     if enter_key == ENTER_KEY:
         files = [{
             "filename": elem.name,
@@ -144,20 +145,11 @@ def get_all(enter_key):
         } for elem in os.scandir(UPLOAD_FOLDER) if elem.is_file()]
 
         return render_template("all_files.html", files=files, upload_folder=UPLOAD_FOLDER)
-        # if os.listdir(UPLOAD_FOLDER):
-        #     return jsonify([{
-        #         "ID": idx,
-        #         "filename": elem.name,
-        #         "filepath": elem.path,
-        #         "filesize(in bytes)": elem.stat().st_size,
-        #     } for idx, elem in enumerate(os.scandir(UPLOAD_FOLDER), start=1) if elem.is_file()]), 200
-        # else:
-        #     return jsonify(info="There is not any file in directory"), 200
     else:
         return render_template("key_error.html"), 400
 
 @app.route("/<string:admin_key>/delete_all", methods=["GET", "DELETE"])
-def delete_all(admin_key):
+def delete_all(admin_key) -> Response:
     if admin_key == ADMIN_KEY:
         try:
             if os.listdir(UPLOAD_FOLDER):
@@ -175,25 +167,25 @@ def delete_all(admin_key):
     else:
         return jsonify(error="Admin key is incorrect:("), 400
 
-@app.route("/404")
-def error_404():
+@app.route("/404") # for testing this template and error code:)
+def error_404() -> Tuple[str, int]:
     return render_template("404.html"), 404
         
 @app.errorhandler(404)
-def page_not_found(error):
+def page_not_found(error) -> Tuple[str, int]:
     return render_template("404.html"), 404
 
 @app.errorhandler(405)
-def method_not_allowed(error):
+def method_not_allowed(error) -> Tuple[str, int]:
     return render_template("405.html"), 405
 
 @app.errorhandler(413)
-def request_entity_too_large(error):
+def request_entity_too_large(error) -> Tuple[str, int]:
     return render_template("413.html"), 413
 
 @app.errorhandler(429)
-def too_many_requests(error):
+def too_many_requests(error) -> Tuple[str, int]:
     return render_template("429.html"), 429
 
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0")
+    app.run(debug=True)
